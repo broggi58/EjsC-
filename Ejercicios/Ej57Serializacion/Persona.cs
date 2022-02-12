b48e70071b17811d047fb2c6b41ab08d00943088
broggi58 <broggi@outlook.com>
2022-02-12 00:47:52 -0300

    Ej 57 Serializacion

---
+++ Ejercicios/Ej57Serializacion/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej57Serializacion
+{
+    class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public Persona ()
+        {
+            
+        }
+
+        public Persona(string nom, string ape)
+        {
+            this.nombre = nom;
+            this.apellido = ape;
+        }
+
+        public static void Guardar()
+        {
+
+        }
+
+    }
+}
---
+++ Ejercicios/Ej57Serializacion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ej57Serializacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
